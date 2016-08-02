4fd475b4e5e5e971323c73659b742441e6bf7f82
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2016-08-02 00:02:53 -0400

    Sequelize setup

---
+++ config/constants.js
@@ -0,0 +1,11 @@
+
+export const SEX_TYPES = {
+  M: 'Male',
+  F: 'Female',
+};
+
+export const PREFERRED_CONTACT_METHODS = {
+  office: 'I will contact the office',
+};
+
+export default {};
--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+require('babel-register');
+
+// Load dotenv if needed
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = path.normalize(path.join(__dirname, '/..'));
+
+// Test if dot env exists.
+if (fs.existsSync(path.join(rootDir, '.env'))) {
+  require('dotenv').load();  // eslint-disable-line global-require
+}
+
+const isDeveloment = process.env.NODE_ENV !== 'production';
+
+
 // Main starting point of the application
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const app = express();
 const router = require('./router');
-const mongoose = require('mongoose');
 
-// DB Setup
-mongoose.connect('mongodb://localhost:auth/auth');
+const app = express();
+
+// require models
+require('./models');
 
 // App Setup
-app.use(morgan('combined'));
-app.use(bodyParser.json({type: '*/*'}));
+app.use(morgan(isDeveloment ? 'dev' : 'combined'));
+app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 // Server Setup
