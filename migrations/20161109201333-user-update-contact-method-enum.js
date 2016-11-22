0a6621c74ef6ad67853150ea2ced158cfc3b3699
Mike O'Dell <mikeodell77@gmail.com>
2016-11-21 20:18:26 -0500

    ugh!

--- migrations/20161109201333-user-update-contact-method-enum.js
+++ migrations/20161109201333-user-update-contact-method-enum.js
@@ -4,7 +4,7 @@ module.exports = {
   up: function (queryInterface) {
     // return queryInterface.sequelize.query("CREATE TYPE \"enum_users_contactMethod\" AS ENUM(\'office\', \'email\', \'phone\'); ALTER TABLE \"users\" ADD COLUMN \"contactMethod\" \"enum_users_contactMethod\";");
     // return queryInterface.sequelize.query("ALTER TYPE \"enum_users_contactMethod\" ADD VALUE \'email\' AFTER \'office\';");
-    return queryInterface.sequelize.query("ALTER TYPE \"enum_users_contactMethod\" ADD VALUE \'phone\' AFTER \'email\';");
+    // return queryInterface.sequelize.query("ALTER TYPE \"enum_users_contactMethod\" ADD VALUE \'phone\' AFTER \'email\';");
 
   },
 
