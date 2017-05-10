920f1f80d1af72b0a0972742d4f8573411ab2830
Victor Igbokwe <trupprdev@gmail.com>
2017-05-11 00:00:15 +0100

    Added email templates for new reviews, new patients and config/service files.

---
+++ config/messages.js
@@ -0,0 +1,22 @@
+export const dentistMessages = {
+  new_patient: `
+    Great News! You have a new member enrolled in your dental membership plan.
+    Please login to your dashboard to review the members information and contact
+    them within 3 business days if they have indicated they would like for you
+    to call/email them for an appointment.
+  `,
+
+  new_review: `
+    *** Feedback Alert ***
+    You have received a review from one of your members please read the review
+    below and login to your dashboard if you wish to respond.
+    This review will be posted onto the marketplace in 7 days.
+  `
+};
+
+export const patientMessages = {
+  welcome: `
+    If you were assisted by your dental office in setting up your account
+    please click here to create a new password.
+  `
+};
