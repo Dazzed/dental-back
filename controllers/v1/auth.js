b23b66ed78dc2836687747c258b51dffdae0f125
Sameep <sameepsheth@gmail.com>
2017-06-21 10:29:02 +0000

    Modified activation_complete email template message. Added logo.

--- controllers/v1/auth.js
+++ controllers/v1/auth.js
@@ -243,7 +243,7 @@ function dentistUserSignup(req, res, next) {
       res
       .status(HTTPStatus.CREATED)
       .json({});
-      Mailer.dentistSignupEmail(res, req.body.user);
+      // Mailer.dentistSignupEmail(res, req.body.user);
     })
     .catch(next);
   })
