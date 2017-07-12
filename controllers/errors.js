4e5ab31921c50f4e356a03ad2a9f3eacaf0f742a
Godson Ukpere <godson.ukpere@gmail.com>
2017-07-12 04:11:53 +0100

    chore(Errors): Expose error message in error response

--- controllers/errors.js
+++ controllers/errors.js
@@ -9,7 +9,7 @@ import HTTPStatus from 'http-status';
 export class ForbiddenError extends Error {
   constructor(message = 'Forbidden') {
     super(message);
-    this.message = message;
+    this.errors = message;
     this.statusCode = HTTPStatus.FORBIDDEN;
   }
 }
@@ -18,9 +18,8 @@ export class BadRequestError extends Error {
   constructor(message = 'Bad Request', errors = null) {
     super(message);
     this.statusCode = HTTPStatus.BAD_REQUEST;
-    this.message = message;
+    this.errors = message;
     if (!(process.env.NODE_ENV === 'production')) {
-      this.errors = errors;
       delete this.meta;
     }
   }
@@ -30,14 +29,14 @@ export class NotFoundError extends Error {
   constructor(message = 'Not Found') {
     super(message);
     this.statusCode = HTTPStatus.NOT_FOUND;
-    this.message = message;
+    this.errors = message;
   }
 }
 
 export class UnauthorizedError extends Error {
   constructor(message = 'Unauthorized Access') {
     super(message);
-    this.message = message;
+    this.errors = message;
     this.statusCode = HTTPStatus.UNAUTHORIZED;
   }
 }
