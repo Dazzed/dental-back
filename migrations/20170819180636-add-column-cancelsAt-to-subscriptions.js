fbf688616a4617d074e124fdafc4bb6ef605b745
Sameep Sheth <sameepsheth@gmail.com>
2017-08-19 23:37:25 +0530

    Fix new migration

--- migrations/20170819180636-add-column-cancelsAt-to-subscriptions.js
+++ migrations/20170819180636-add-column-cancelsAt-to-subscriptions.js
@@ -6,6 +6,7 @@ module.exports = {
 
     return sequelize.transaction(t => {
       return Promise.all([
+        sequelize.query('ALTER TABLE subscriptions ADD COLUMN "cancelsAt" TIMESTAMP NULL DEFAULT NULL', { transaction: t }),
         sequelize.query('ALTER TYPE enum_subscriptions_status RENAME TO _enum_sub_status', { transaction: t }),
         sequelize.query(`CREATE TYPE enum_subscriptions_status AS ENUM ('trialing', 'active', 'inactive', 'past_due', 'canceled', 'unpaid', 'expired', 'cancellation_requested')`, { transaction: t }),
         sequelize.query('ALTER TABLE subscriptions RENAME COLUMN status TO _status', { transaction: t }),
@@ -13,7 +14,6 @@ module.exports = {
         sequelize.query('UPDATE subscriptions SET status = _status::text::enum_subscriptions_status', { transaction: t }),
         sequelize.query('ALTER TABLE subscriptions DROP COLUMN _status', { transaction: t }),
         sequelize.query('DROP TYPE _enum_sub_status', { transaction: t }),
-        sequelize.query('ALTER TABLE subscriptions ADD COLUMN "cancelsAt" TIMESTAMP NULL DEFAULT NULL')
       ]);
     });
   },
@@ -23,6 +23,7 @@ module.exports = {
 
     return sequelize.transaction(t => {
       return Promise.all([
+        sequelize.query('ALTER TABLE subscriptions DROP COLUMN "cancelsAt"', {transaction: t}),
         sequelize.query('ALTER TYPE enum_subscriptions_status RENAME TO _enum_sub_status', { transaction: t }),
         sequelize.query(`CREATE TYPE enum_subscriptions_status AS ENUM ('trialing', 'active', 'inactive', 'past_due', 'canceled', 'unpaid', 'expired')`, { transaction: t }),
         sequelize.query('ALTER TABLE subscriptions RENAME COLUMN status TO _status', { transaction: t }),
@@ -30,7 +31,6 @@ module.exports = {
         sequelize.query('UPDATE subscriptions SET status = _status::text::enum_subscriptions_status', { transaction: t }),
         sequelize.query('ALTER TABLE subscriptions DROP COLUMN _status', { transaction: t }),
         sequelize.query('DROP TYPE _enum_sub_status', { transaction: t }),
-        sequelize.query('ALTER TABLE subscriptions DROP COLUMN cancelsAt')
       ]);
     });
   }
