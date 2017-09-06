6eeef828920440b95d25b4e7f83cd6f3e4ae9964
Sameep <sameepsheth@gmail.com>
2017-09-06 16:22:40 +0000

    Clean up general reports

--- controllers/v1/reports.js
+++ controllers/v1/reports.js
@@ -67,7 +67,7 @@ function getListOfReportURLs(req, res) {
  * @param {Object} req - the express request
  * @param {Object} res - the express response
  */
-function getGeneralReport(req, res) {
+function getGeneralReportOld(req, res) {
   const reportEndDate = new Moment();
   reportEndDate.year(req.params.year).month(req.params.month).date(1).subtract(1, 'day');
 
@@ -242,106 +242,127 @@ function getGeneralReport(req, res) {
  * @param {Object} req - the express request
  * @param {Object} res - the express response
  */
-function getMasterReport(req, res, next) {
-  const month = Moment.monthsShort()[req.params.month];
+async function getMasterReport(req, res) {
+  try {
+    let targetMonth = Moment.monthsShort().indexOf(req.params.month);
+    if (targetMonth === -1) {
+      throw { errors: 'Invalid Month given' };
+    }
 
-  db.DentistInfo.findAll({
-    include: [
-      { model: db.User, as: 'user' },
-      { model: db.Membership, as: 'childMembership' },
-      { model: db.Membership, as: 'membership' }
-    ]
-  }).then((offices) => {
-    // Collect office reports
-    Promise.all(offices.map(office => new Promise((resolve, reject) => {
-      // Validate the current user has the proper authorization to see this report
-      db.Subscription.findAll({
-        where: { dentistId: office.user.id },
-        include: [{
-          model: db.User,
-          as: 'client',
-        }, {
-          model: db.Membership,
-          as: 'membership',
-        }],
-      }).then((members) => {
-        // Calculate gross revenue and prepare member records
-        const gross = members.reduce((sum, m) => {
-          let fee = 0;
+    const fullMonthName = Moment.months()[targetMonth];
+    targetMonth += 1;
+    if (targetMonth < 10) {
+      targetMonth = String(`0${targetMonth}`);
+    }
+    const targetYear = req.params.year;
+    const targetDate = Moment(`${targetYear}-${targetMonth}-01`, 'YYYY-MM-DD');
+    const targetDateCopy = Moment(targetDate);
+    const daysInTargetMonth = Moment(`${targetYear}-${targetMonth}`,'YYYY-MM').daysInMonth();
 
-          if (office.acceptsChildren && m.membership.childYearlyFeeActivated) {
-            // Child pricing
-            fee = parseInt(office.childMembership.monthly, 10);
-          } else {
-            // Adult pricing
-            fee = parseInt(office.membership.monthly, 10);
-          }
+    let dentists = await db.DentistInfo.findAll({
+      include:[{
+        model: db.User,
+        as: 'user'
+      }]
+    }).map(t => t.toJSON());
+    
+    const payments = await db.Payment.findAll({
+      where: {
+        createdAt: {
+          $between: [targetDate.format('YYYY-MM-DD'), targetDateCopy.set('date', daysInTargetMonth).format('YYYY-MM-DD')]
+        }
+      }
+    }).map(t => t.toJSON());
 
-          return (sum + fee);
-        }, 0);
+    const datePeriod = `${fullMonthName} ${targetYear}`;
 
-        const managementFee = (gross * 0.11);
-
-        resolve({ officeName: office.officeName, gross, managementFee, net: (gross - managementFee) });
-      }).catch(err => reject(err));
-    }))).then((officeReports) => {
-      // Calculate totals
-      const totals = officeReports.reduce((t, office) => {
-        t.gross += office.gross;
-        t.managementFee += office.managementFee;
-        t.net += office.net;
-        return t;
-      }, {
-        gross: 0,
-        managementFee: 0,
-        net: 0,
-      });
-
-      // Round off the totals
-      totals.gross = Number(totals.gross.toFixed(2));
-      totals.managementFee = Number(totals.managementFee.toFixed(2));
-      totals.net = Number(totals.net.toFixed(2));
-
-      // Fetch the master template file
-      const masterTemplate = fs.readFileSync(
-        path.resolve(`${__dirname}/../../views/reports/master-report.html`),
-        'utf8'
-      );
-
-      // Compose the master report
-      const report = nunjucks.renderString(masterTemplate, {
-        datePeriod: `${month} ${req.params.year}`,
-        records: officeReports,
-        totals,
-      });
-
-      // Send PDF file
-      pdf.create(report, { format: 'Letter' }).toBuffer((err, resp) => {
-        if (err) { res.json(new BadRequestError(err)); }
-        res.writeHead(200, { 'Content-Type': 'application/pdf' });
-        res.write(resp);
-        res.end();
-      });
+    dentists = dentists.map(dentist => {
+      const filteredPayments = payments.filter(payment => payment.dentistId === dentist.id);
+      const gross = filteredPayments.reduce((acc, p) => acc += p.amount, 0);
+      const managementFee = gross * (11 / 100);
+      const net = gross - managementFee;
+      return {
+        ...dentist,
+        gross: Math.round(gross / 100, 2),
+        managementFee: Math.round(managementFee / 100, 2),
+        net: Math.round(net / 100, 2)
+      };
     });
-  }).catch(err => next(new BadRequestError(err)));
-}
 
-async function getMasterReportCleaned(req, res) {
-  
+    const totals = dentists.reduce((acc, dentist) => {
+      const { gross, managementFee, net } = acc;
+      const { gross: dentistGross, managementFee: dentistManagementFee, net: dentistNet } = dentist;
+      return {
+        gross: gross + dentistGross,
+        managementFee: managementFee + dentistManagementFee,
+        net: net + dentistNet
+      };
+    }, { gross: 0, managementFee: 0, net: 0 });
+
+    // Fetch the master template file
+    const masterTemplate = fs.readFileSync(
+      path.resolve(`${__dirname}/../../views/reports/master-report.1.html`),
+      'utf8'
+    );
+
+    // Compose the master report
+    const report = nunjucks.renderString(masterTemplate, {
+      datePeriod,
+      records: dentists,
+      totals,
+    });
+
+    // Send PDF file
+    pdf.create(report, { format: 'Letter' }).toBuffer((err, resp) => {
+      if (err) { res.json(new BadRequestError(err)); }
+      res.writeHead(200, { 'Content-Type': 'application/pdf' });
+      res.write(resp);
+      res.end();
+    });
+
+  } catch (e) {
+    console.log('Error in getMasterReportCleaned');
+    console.log(e);
+    return res.status(500).send({ errors: 'Internal Server Error' });
+  }
 }
 
 // ────────────────────────────────────────────────────────────────────────────────
 // ENDPOINTS
 
-async function getReport(req,res) {
+async function getGeneralReport(req,res) {
   try {
     const { dentistId } = req.params;
     const dentist = await db.User.findOne({ where: { id: dentistId } });
     const dentistInfo = await db.DentistInfo.findOne({ where: { userId: dentistId } });
-    const dentistSpecialityName = 'GENERAL DENTIST';
-    const date = 'SEP 2017';
+    const { name: dentistSpecialityName } = await db.DentistSpecialty.findOne({ where: { id: dentist.dentistSpecialtyId } });
 
-    const dentistSubscriptions = await db.Subscription.findAll({ where: { dentistId } })
+    // BEGIN Date Ops
+    let targetMonth = Moment.monthsShort().indexOf(req.params.month);
+    if (targetMonth === -1) {
+      throw { errors: 'Invalid Month given' };
+    }
+
+    const fullMonthName = Moment.months()[targetMonth];
+    targetMonth += 1;
+    if (targetMonth < 10) {
+      targetMonth = String(`0${targetMonth}`);
+    }
+    const targetYear = req.params.year;
+    const targetDate = Moment(`${targetYear}-${targetMonth}-01`, 'YYYY-MM-DD');
+    const targetDateCopy = Moment(targetDate);
+    const daysInTargetMonth = Moment(`${targetYear}-${targetMonth}`,'YYYY-MM').daysInMonth();
+    // END Date Ops
+
+    const date = `${fullMonthName} ${targetYear}`;
+    const dentistSubscriptions = await db.Subscription.findAll({
+      where: {
+        dentistId,
+        createdAt: {
+          $between: [targetDate.format('YYYY-MM-DD'), targetDateCopy.set('date', daysInTargetMonth).format('YYYY-MM-DD')]
+        }
+      }
+    });
 
     const totalMembers = dentistSubscriptions.length;
     const totalExternal = 0;
@@ -350,13 +371,34 @@ async function getReport(req,res) {
     const totalNewExternal = 0;
     const totalNewInternal = totalMembers;
 
-    const payments = await db.Payment.findAll({ where: { dentistId } });
+    const payments = await db.Payment.findAll({ 
+      where: {
+        dentistId,
+        createdAt: {
+          $between: [targetDate.format('YYYY-MM-DD'), targetDateCopy.set('date', daysInTargetMonth).format('YYYY-MM-DD')]
+        }
+      }
+    });
     const grossRevenue = payments.reduce((acc,p) => acc += Math.round(p.amount / 100, 2), 0);
 
-    const refundsRecords = await db.Refund.findAll({ where: { dentistId } });
+    const refundsRecords = await db.Refund.findAll({
+      where: {
+        dentistId,
+        createdAt: {
+          $between: [targetDate.format('YYYY-MM-DD'), targetDateCopy.set('date', daysInTargetMonth).format('YYYY-MM-DD')]
+        }
+      }
+    });
     const refunds = refundsRecords.reduce((acc,p) => acc += Math.round(p.amount / 100, 2), 0);
 
-    const penaltiesRecords = await db.Penality.findAll({ where: { dentistId } });
+    const penaltiesRecords = await db.Penality.findAll({
+      where: {
+        dentistId,
+        createdAt: {
+          $between: [targetDate.format('YYYY-MM-DD'), targetDateCopy.set('date', daysInTargetMonth).format('YYYY-MM-DD')]
+        }
+      }
+    });
     const penalties = penaltiesRecords.reduce((acc,p) => acc += Math.round(p.amount / 100, 2), 0);
     
     const managementFee = Math.round(grossRevenue * (11 / 100), 2);
@@ -368,12 +410,12 @@ async function getReport(req,res) {
           acc.push(s.paymentProfileId);
         }
         return acc;
-      },[]);
-    const paymentProfileRecords = await db.PaymentProfile.findAll({ where: { 
+      }, []);
+    const paymentProfileRecords = await db.PaymentProfile.findAll({ where: {
       id: filteredPaymentProfileIds
-    }});
+    } });
 
-    let parentMemberRecords = [];
+    const parentMemberRecords = [];
 
     for (let profile of paymentProfileRecords) {
       let parentLocal = {};
@@ -383,7 +425,9 @@ async function getReport(req,res) {
       parentLocal.firstName = parent.firstName;
       parentLocal.lastName = parent.lastName;
       parentLocal.maturity = 'Adult';
-      parentLocal.fee = Math.round(payments.find(p => p.clientId == parent.id).amount / 100, 2);
+      const parentFee = payments.find(p => p.clientId == parent.id);
+      parentLocal.fee = parentFee ? Math.round(parentFee.amount / 100, 2) : 0;
+      
       parentLocal.penalties = penaltiesRecords.filter(p => p.clientId == parent.id)
         .reduce((acc, p) => acc += Math.round(p.amount / 100, 2) ,0);
       parentLocal.refunds = refundsRecords.filter(p => p.clientId == parent.id)
@@ -392,16 +436,17 @@ async function getReport(req,res) {
 
       parentLocal.family = [];
       childMembers.forEach(child => {
-        let childLocal = {};
+        const childLocal = {};
         childLocal.firstName = child.firstName;
         childLocal.lastName = child.lastName;
-        childLocal.maturity = Moment().subtract(Moment(child.birthDate,'YYYY-MM-DD'), 'years') >= 18 ? 'Adult' : 'Child';
-        childLocal.fee = Math.round(payments.find(p => p.clientId == child.id).amount / 100, 2);
+        childLocal.maturity = Moment('YYYY-MM-DD').subtract(Moment(child.birthDate, 'YYYY-MM-DD'), 'years') >= 18 ? 'Adult' : 'Child';
+        const childLocalFee = payments.find(p => p.clientId == child.id);
+        childLocal.fee = childLocalFee ? Math.round(childLocalFee.amount / 100, 2) : 0;
         childLocal.penalties = penaltiesRecords.filter(p => p.clientId == child.id)
           .reduce((acc, p) => acc += Math.round(p.amount / 100, 2) ,0);
         childLocal.refunds = refundsRecords.filter(p => p.clientId == child.id)
           .reduce((acc, p) => acc += Math.round(p.amount / 100, 2) ,0);
-        childLocal.net = Math.round(childLocal.fee - childLocal.fee * (11/100), 2);
+        childLocal.net = Math.round(childLocal.fee - (childLocal.fee * (11 / 100)), 2);
         parentLocal.family.push(childLocal);
       });
       parentLocal.membershipFeeTotal = payments
@@ -413,13 +458,14 @@ async function getReport(req,res) {
       parentLocal.refundsTotal = refundsRecords
         .filter(p => p.clientId == parent.id || childMembers.map(c => c.id).includes(p.clientId))
         .reduce((acc, p) => acc += Math.round(p.amount / 100, 2) ,0);
-      parentLocal.netTotal = Math.round(parentLocal.membershipFeeTotal - parentLocal.membershipFeeTotal * (11/100), 2);
+      parentLocal.netTotal = Math.round(parentLocal.membershipFeeTotal - (parentLocal.membershipFeeTotal * (11 / 100)), 2);
       parentMemberRecords.push(parentLocal);
     }
-    console.log(parentMemberRecords);
+
     const reportData = {
       title: `${dentistInfo.officeName} -- General Report`,
-      date: `${Moment.months(new Moment().month())} ${new Moment().format('YYYY')}`,
+      dentistSpecialityName,
+      date,
       totalMembers: parentMemberRecords.length,
       parentMemberRecords,
       totalNewMembers,
@@ -468,7 +514,8 @@ function getMasterDates(req, res) {
     dates[year].push({
       month,
       year,
-      url: `/reports/master/${year}/${monthShort}`
+      monthShort,
+      url: `admin/master_report/${year}/${monthShort}`
     });
 
     initialDate.add('1', 'month');
@@ -477,11 +524,6 @@ function getMasterDates(req, res) {
 }
 
 const router = new Router({ mergeParams: true });
-
-// router.route('/:dentistId')
-//   .get(
-//     getReport
-//   );
 
 router.route('/master_dates')
   .get(
@@ -497,22 +539,16 @@ router.route('/dentist/dates/:officeId/list')
     injectDentistOffice('officeId', 'office'),
     getListOfReportURLs);
 
-router.route('master_report/:year/:month')
-  .get(
-    userRequired,
-    getMasterReportCleaned
-  );
-
-router.route('/dentist/:officeId/:year/:month/general')
-  .get(
-    userRequired,
-    dentistRequired,
-    getGeneralReport);
-
-router.route('/dentists/:year/:month')
+router.route('/admin/master_report/:year/:month')
   .get(
     userRequired,
     adminRequired,
-    getMasterReport);
+    getMasterReport
+  );
+
+router.route('/dentist/:dentistId/general_report/:year/:month')
+  .get(
+    getGeneralReport
+  );
 
 export default router;
