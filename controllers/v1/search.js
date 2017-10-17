c33ee7f7edb9e401ca7fb277b93cd95e6e46c944
dazzed <sameepsheth@gmail.com>
2017-10-17 14:11:36 +0000

    remove coordinates reading from FE

--- controllers/v1/search.js
+++ controllers/v1/search.js
@@ -15,7 +15,6 @@ async function search(req, res) {
       distance,
       sort,
       specialties,
-      coordinates,
     } = filters;
     let sequelizeDistance;
     let dentists = [];
@@ -35,24 +34,8 @@ async function search(req, res) {
       } else {
         return res.status(400).send({ errors: 'Please Enter a valid search query' });
       }
-    } else if (!searchQuery && !distance) {
-      whereClause = {};
     } else {
-      // Exception
-      if (!coordinates) {
-        return res.status(200).send({ dentists });
-      }
-      const {
-        lat,
-        lng
-      } = coordinates;
-      if (!lat || !lng) {
-        return res.status(200).send({ dentists });
-      }
-      // End Exception
-      const location = sequelize.literal(`ST_GeomFromText('POINT(${lat} ${lng})')`);
-      sequelizeDistance = sequelize.fn('ST_Distance_Sphere', sequelize.col('location'), location);
-      whereClause = sequelize.where(sequelizeDistance, { $lte: Number(distance) * 1000 });
+      whereClause = {};
     }
     dentists = await db.DentistInfo.findAll({
       order: sequelizeDistance,
@@ -76,8 +59,8 @@ async function search(req, res) {
       }]
     }).map(d => d.toJSON());
     // Specialties filter
-    if (specialties.length > 0) {
-      dentists = dentists.filter(d => specialties.includes(d.user.dentistSpecialtyId));
+    if (specialties) {
+      dentists = dentists.filter(d => specialties == (d.user.dentistSpecialtyId));
     }
     // construct starting price for every dentist..
     dentists = dentists
