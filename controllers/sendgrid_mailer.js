9951aec7c37a032a1cce18fa5e8268a95924bd67
dazzed <sameepsheth@gmail.com>
2017-10-23 13:18:24 +0000

    177

--- controllers/sendgrid_mailer.js
+++ controllers/sendgrid_mailer.js
@@ -1,3 +1,5 @@
+const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
 var sendgrid = require('sendgrid').mail;
 var ejs = require('ejs');
 var fs = require('fs')
@@ -6,10 +8,9 @@ import {
   EMAIL_SUBJECTS,
 } from '../config/constants';
 
-var from_email = new sendgrid.Email("donotreply@dentalhq.com");
+let from_email = new sendgrid.Email("donotreply@dentalhq.com");
 
 function sendMail(mail) {
-  let sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
   let request = sg.emptyRequest({
     method: 'POST',
     path: '/v3/mail/send',
@@ -26,15 +27,16 @@ function sendMail(mail) {
   });
 }
 
-export function membershipPriceChangeNotificationAdvance(user, plan_name, price) {
+export function membershipPriceChangeNotificationAdvance(user, plan_name, price, officeName, officeInfo) {
   var templateString = fs.readFileSync('./views/notifications/membership_price_update_advance.ejs', 'utf-8');
 
   var template = ejs.compile(templateString);
   var priceS = price.toString();
+  from_email = new sendgrid.Email(`donotreply@${officeName}.com`);
   let to_email = new sendgrid.Email(user.email);
   let subject = EMAIL_SUBJECTS.client.membershipPriceUpdate;
   let content = new sendgrid.Content(
-    'text/html', template({ user, plan_name, priceS })
+    'text/html', template({ user, plan_name, price: priceS, subject, officeName, dentistInfo: officeInfo })
   );
   let mail = new sendgrid.Mail(from_email, subject, to_email, content);
   sendMail(mail);
@@ -49,7 +51,7 @@ export function membershipPriceChangeNotification(user, plan_name, price) {
   let to_email = new sendgrid.Email(user.email);
   let subject = EMAIL_SUBJECTS.client.membershipPriceUpdate;
   let content = new sendgrid.Content(
-    'text/html', template({ user, plan_name, priceS })
+    'text/html', template({ user, plan_name, price: priceS, subject })
   );
   let mail = new sendgrid.Mail(from_email, subject, to_email, content);
   sendMail(mail);
@@ -73,20 +75,20 @@ export function subscriptionChargeFailedNotification(user, attempt_count) {
     days_late = 21;
   }
   let content = new sendgrid.Content(
-    'text/html', template({ user, days_late })
+    'text/html', template({ user, days_late, subject })
   );
   let mail = new sendgrid.Mail(from_email, subject, to_email, content);
   sendMail(mail);
 }
 
-export function subscriptionCancellationNotification(user) {
+export function subscriptionCancellationNotification(user, officeName) {
   var templateString = fs.readFileSync('./views/notifications/subscription_cancellation.ejs', 'utf-8');
   var template = ejs.compile(templateString);
-
+  from_email = new sendgrid.Email(`donotreply@${officeName}.com`);
   let to_email = new sendgrid.Email(user.email);
   let subject = EMAIL_SUBJECTS.client.subscriptionCancellation;
   let content = new sendgrid.Content(
-    'text/html', template({ user})
+    'text/html', template({ user, subject })
   );
   let mail = new sendgrid.Mail(from_email, subject, to_email, content);
   sendMail(mail);
