5b3f90ec256d06712af09bb9cc5565d084d9e888
dazzed <sameepsheth@gmail.com>
2017-10-27 11:48:02 +0000

    change admin password

--- helpers/managers.js
+++ helpers/managers.js
@@ -1,3 +1,5 @@
+import passport from 'passport';
+
 import db from '../models';
 import {
   sendTermsAndConditionsUpdatedEmail
@@ -24,11 +26,38 @@ async function termsAndConditionsUpdateNotification() {
   });
 
   const users = dentists.concat(customers);
-  users.forEach(user => {
+  users.forEach((user) => {
     sendTermsAndConditionsUpdatedEmail(user.firstName, user.email);
   });
 }
 
+function isValidChangePasswordObject(req, res, next) {
+  const {
+    password,
+    newPassword,
+    confirmNewPassword
+  } = req.body;
+  if (!password || !newPassword || !confirmNewPassword) {
+    return res.status(400).send({ errors: 'Missing params' });
+  }
+
+  if (newPassword !== confirmNewPassword) {
+    return res.status(400).send({ errors: 'Passwords do not match' });
+  }
+  next();
+}
+
+async function isCurrentPasswordValid(req, res, next) {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).send({ errors: 'Invalid Old Password' });
+    }
+    next();
+  })(req, res, next);
+}
+
 export {
-  termsAndConditionsUpdateNotification
+  termsAndConditionsUpdateNotification,
+  isValidChangePasswordObject,
+  isCurrentPasswordValid
 };
